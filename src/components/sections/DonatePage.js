94990542ee43a69217f49f979907247ebeff0997
Jatin <75193966+TEDDY6969@users.noreply.github.com>
2022-10-05 20:50:05 +0530

    Update DonatePage.js

--- src/components/sections/DonatePage.js
+++ src/components/sections/DonatePage.js
@@ -17,16 +17,16 @@ const DonatePage = () => {
       <Typography variant="subtitle1" align="center">
         This app is free and open source for everyone and It doesn't show any
         ads because I wanted to keep it like that, But there are servers running
-        and many other things which comes to a cost and I need to pay for those and also I need to buy a secure domain too. so, I would be so grateful is you could donate me somewhat bucks.
+        and many other things which comes to a cost and I need to pay for those. So, I would be so grateful is you could donate me somewhat bucks.
         
         It would be really helpful if you could donate and help me keep doing
         what I do. Thanks…
         <br/>
-          You can pay anything upto your will on this no.
+          You can pay anything upto your will here on this no..
           <br/>
             +91-9828243135 (for indians)
            <br/>
-            If you nri, please consider to sponsor me on GitHub.
+            If you are a nri, please consider to sponsor me on GitHub.
       </Typography>
       
     </main>
