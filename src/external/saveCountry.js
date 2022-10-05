1f8af52191893c7e0a08370b30d10c51fa638935
Jatin <75193966+TEDDY6969@users.noreply.github.com>
2022-10-05 10:57:16 +0530

    Add files via upload

---
+++ src/external/saveCountry.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const country_code = localStorage.getItem("country_code");
+
+// const key = "02924c9a5a777f4d4854a45a326432c6";
+
+const GeoAPI = "https://ipapi.co/json/";
+
+const fetchCountry = async () => {
+  const res = await axios.get(GeoAPI, { mode: "no-cors" });
+  //   set the current country code in local stoarge
+  localStorage.setItem("country_code", res.data.country);
+};
+
+if (!country_code) {
+  // if country is not set then only set it
+  fetchCountry();
+}
