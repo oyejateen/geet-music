54f5c1147d17808658a56bd5d1ff9e30d3ac5da3
Jatin <75193966+TEDDY6969@users.noreply.github.com>
2022-10-06 10:51:04 +0530

    Update getAudioLink.js

--- src/apis/getAudioLink.js
+++ src/apis/getAudioLink.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 /*const instance = axios.create({
-     baseURL: 'https://Ylight-Music.elobhi.repl.co/api/',
-  // baseURL: 'https://ylight.glitch.me',
+     
 });*/
 export default axios.create({
-  baseURL: "https://server-teddy6969.vercel.app/",
+  baseURL: "https://ylight-music-api.vercel.app",
 });
 //instance.defaults.headers.common['Authorization'] = "AIzaSyBh3IuBJGuNTYp70xeMSUpOBMmW2S5gFSA";
