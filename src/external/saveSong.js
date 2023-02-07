d22cb62ab13f0906a628474dc0d7c9e7d01d8110
Jatin <75193966+TEDDY6969@users.noreply.github.com>
2023-02-07 14:38:22 +0530

    Update saveSong.js

--- src/external/saveSong.js
+++ src/external/saveSong.js
@@ -118,29 +118,18 @@ export const deleteSongAudio = async (id) => {
   return 'song deleted';
 };
 
-function fetchProxiedBlob(url) {
+async function fetchProxiedBlob(url) {
   const URL = url;
   return new Promise(function (resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', "https://jatinxd.tk/");
-    xhr.responseType = 'blob';
-    xhr.onload = function () {
-      var status = xhr.status;
-      if (status >= 200 && status < 300) {
-        resolve(xhr.response);
-      } else {
-        reject({
-          status: status,
-          statusText: xhr.statusText,
-        });
-      }
-    };
-    xhr.send();
-    setTimeout(() => {
-      xhr.abort();
-      xhr.open('GET', "https://jatinxd.tk/");
-
-      xhr.send();
-    }, 1000);
+    fetch(`https://server-geet.iiiv.repl.co/proxy/${URL}`)
+      .then(response => {
+        
+        if (!response.ok) {
+          console.error(`HTTP error! status: ${response.status}`);
+        }
+        return response.blob();
+      })
+      .then(blob => resolve(blob))
+      .catch(error => reject(error));
   });
 }
